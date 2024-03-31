d72fb25040324f9e8259713df4469fcd651256bf
rbaker4559 <reginaldsbaker10@icloud.com>
2024-03-30 20:10:24 -0700

    debugging api routes by removing dupe sends

--- Develop/server.js
+++ Develop/server.js
@@ -8,9 +8,10 @@ const fs = require('fs');
 const router = express.Router();
 
 //middleware
+app.use(express.json());
 app.use(express.static('public'));
 
-app.get("/", (req, res) => res.send('index.html'),); //need to check work here
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html'))); //need to check work here
 
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'notes.html'))
@@ -25,7 +26,6 @@ app.get("/api/notes", (req,res) => {
         }
         const notes = JSON.parse(data);
         res.json(notes);
-
     });
 });
 
@@ -37,15 +37,38 @@ app.post('/api/notes', (req, res) => {
             res.status(500).send('internal server error');
             return;
         }
-        const notes = JSON.parse(notes);
-        newNotes.id = Math.floor(Math.floor() * 1000000);
+        const notes = JSON.parse(data);
+        newNote.id = Math.floor(Math.random() * 1000000);
         notes.push(newNote);
         fs.writeFile(path.join(__dirname, 'db', 'db.json'), JSON.stringify(notes), 'utf8', (err, data) => {
+            if(err){
+                console.error(err);
+                res.status(500).send('internal server error');
+                return;    
+            }
+            res.json(newNote);
+        });
+    });
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+    const noteID = req.params.id;
+    fs.readFile(path.join(__dirname, 'db', 'db.json'), 'utf8', (err, data) => {
+        if(err){
             console.error(err);
             res.status(500).send('internal server error');
             return;
-        })
-        res.json(newNote);
+        }
+        let notes = JSON.parse(data);
+        notes = notes.filter(note => note.id != noteID);
+        fs.writeFile(path.join(__dirname, 'db', 'db.json'), JSON.stringify(notes), 'utf8', (err) => {
+            if(err){
+                console.error(err);
+                res.status(500).send('internal server error');
+                return;
+            }
+            res.sendStatus(200)
+        });
     });
 });
 
